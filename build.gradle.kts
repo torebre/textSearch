00e8f3f80a5aeb76d7c715c627ec6d7648a4879e
Tore Brede <tore.brede@gmail.com>
2026-09-06 22:23:52 +0200

    Updating to support Java 26

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.compose")
     id("org.jetbrains.compose")
 }
 
@@ -15,36 +16,32 @@ repositories {
 }
 
 kotlin {
-    jvm {
-        jvmToolchain(20)
-        withJava()
-    }
+    jvm()
     sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
 
-                implementation("org.apache.lucene:lucene-core:7.7.3")
-                implementation("org.apache.lucene:lucene-queryparser:7.7.3")
-                implementation("org.apache.lucene:lucene-suggest:7.7.3")
-                implementation("org.apache.lucene:lucene-codecs:7.7.3")
-                implementation("org.apache.lucene:lucene-highlighter:7.7.3")
+            implementation("org.apache.lucene:lucene-core:7.7.3")
+            implementation("org.apache.lucene:lucene-queryparser:7.7.3")
+            implementation("org.apache.lucene:lucene-suggest:7.7.3")
+            implementation("org.apache.lucene:lucene-codecs:7.7.3")
+            implementation("org.apache.lucene:lucene-highlighter:7.7.3")
 
-                implementation("org.slf4j:slf4j-api:1.7.30")
-                implementation("ch.qos.logback:logback-classic:1.2.3")
-                implementation("ch.qos.logback:logback-core:1.2.3")
+            implementation("org.slf4j:slf4j-api:1.7.30")
+            implementation("ch.qos.logback:logback-classic:1.2.3")
+            implementation("ch.qos.logback:logback-core:1.2.3")
 
-                implementation("org.jfree:jfreechart:1.5.4")
-            }
+            implementation("org.jfree:jfreechart:1.5.4")
         }
-        val jvmTest by getting {
-            dependencies {
-                implementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-
-            }
+        jvmTest.dependencies {
+            implementation("org.junit.jupiter:junit-jupiter:5.11.4")
+            implementation("org.junit.platform:junit-platform-launcher")
         }
     }
+}
 
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 compose.desktop {
